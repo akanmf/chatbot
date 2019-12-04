5ff4879864a5ed7ac1012037ded3e53cbc319c61
mbozsarac <54305091+mbozsarac@users.noreply.github.com>
2019-12-04 15:21:04 +0300

    E - mail with Regex

---
+++ EmailValidator.cs
@@ -0,0 +1,13 @@
+﻿using System.Text.RegularExpressions;
+
+namespace Daimler.Bot.PasswordReset.Bot.Validators
+{
+    public class EmailValidator
+    {
+        public static bool Validate(string email)
+        {   //EMAİL KONTROLÜ
+            Regex rg = new Regex(@".{2}\@\w+\.com");
+            return rg.IsMatch(email);
+        }
+    }
+}
